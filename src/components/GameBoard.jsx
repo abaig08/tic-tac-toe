2161397ccf5fc3b91d9efc193cbec95d5f2bd963
Abdullah Baig <baigabdullah174@gmail.com>
2024-05-20 12:11:19 +0530

    fin.

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const GameBoard = ({ onSelectSquare, board }) => {
+  return (
+    <ol id="game-board">
+      {board.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, columnIndex) => (
+              <li key={columnIndex}>
+                <button
+                  onClick={() => onSelectSquare(rowIndex, columnIndex)}
+                  disabled={playerSymbol !== null}
+                >
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+};
+
+export default GameBoard;
